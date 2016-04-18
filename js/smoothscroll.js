1c825210e2a7c8155e1c928a2a7a7538ba121a71
todoapam <todoapam@gmail.com>
2016-04-18 20:55:26 -0300

    Añadido Vídeo

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,6 +1,7 @@
 // smoothscroll
 //http://stackoverflow.com/questions/14804941/how-to-add-smooth-scrolling-to-bootstraps-scroll-spy-function
 
+// anima los botones del menú
 $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
@@ -21,7 +22,29 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
 
 });
 
+// anima el logo
 $("a.navbar-brand[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
+     }, 700, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+// botón para volver hacia arriba
+$("#back-to-top a[href^='#']").on('click', function(e) {
 
    // prevent default anchor click behavior
    e.preventDefault();
